998385181d42b2fccf37fdfe08b6dceb11cfcf2b
Francis Calizo <francisacalizo@gmail.com>
2019-11-30 17:45:54 -0500

    1:18 Hash passwords

--- routes/users.js
+++ routes/users.js
@@ -21,13 +21,19 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('Passed');
+    const {name, email, password} = req.body;
+
+    try {
+      let user = await
+    } catch (err) {
+
+    }
   }
 );
 
